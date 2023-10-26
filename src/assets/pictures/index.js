e7f3ee937478e65dfe96100ac18de19705587917
sam <sammsfs@gmail.com>
2023-10-26 19:44:03 -0400

    fixed image file type again

--- src/assets/pictures/index.js
+++ src/assets/pictures/index.js
@@ -2,7 +2,7 @@ import passwordImage from './password.PNG';
 import jateImage from './jate.PNG';
 import noteImage from './note.PNG';
 import forecastImage from './forecast.png';
-import plannerImage from './planner.PNG';
+import plannerImage from './planner.png';
 import quizImage from './quiz.PNG';
 import cookbookImage from './cookbook.PNG';
 

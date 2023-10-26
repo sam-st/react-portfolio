b9d62b1aafc008b76c02c07ac10f165c803ef275
sam <sammsfs@gmail.com>
2023-10-26 19:42:39 -0400

    fixed image file type

--- src/assets/pictures/index.js
+++ src/assets/pictures/index.js
@@ -1,10 +1,10 @@
 import passwordImage from './password.PNG';
 import jateImage from './jate.PNG';
-import noteImage from './note.PNG'
-import forecastImage from './forecast.PNG'
-import plannerImage from './planner.PNG'
-import quizImage from './quiz.PNG'
-import cookbookImage from './cookbook.PNG'
+import noteImage from './note.PNG';
+import forecastImage from './forecast.png';
+import plannerImage from './planner.PNG';
+import quizImage from './quiz.PNG';
+import cookbookImage from './cookbook.PNG';
 
 export default {
   passwordImage,
